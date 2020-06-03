c1473c29b43fd6f660ba3ed697e94536a9bdc39a
rzhao28 <arenzi28@outlook.com>
2020-06-03 14:21:38 -0400

    -

--- nightly/autotix_nightly.py
+++ nightly/autotix_nightly.py
@@ -15,3 +15,22 @@ bway_txt = "".join([i for i in bway_txt]).replace(" on Broadway", "")
 nightly_file = open("./Excel/AllSalesTest_Updated.csv", "w")
 nightly_file.writelines(bway_txt)
 nightly_file.close()
+'''
+example_file = open("./Excel/AllSalesTest_Updated.csv")
+example_reader = csv.DictReader(example_file)
+example_list = list(example_reader)
+
+output_file = open("./Excel/AllSalesTest_Updated2.csv", "w", newline="")
+output_dict_writer = csv.DictWriter(output_file, ['Name', 'Show', 'Performance Date', 'Confirmation Date',
+                                                  '# of Seats', 'Section', 'Row', 'Start', 'End'])
+
+prev_show = example_list[0]['Show']
+
+for row in example_list:
+    current_show = row['Show']
+    if current_show == prev_show:
+        continue
+    else:
+        prev_show = current_show
+        output_dict_writer.writerow({'Name': ''})
+'''
