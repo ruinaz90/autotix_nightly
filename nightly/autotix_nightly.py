0366aead37e5a8b13e8c92999ae34546d45c10e3
rzhao28 <arenzi28@outlook.com>
2020-06-03 12:49:04 -0400

    -

--- nightly/autotix_nightly.py
+++ nightly/autotix_nightly.py
@@ -1,8 +1,10 @@
 #! python3
-# autotix_nightly.py - Automate nightly
+# autotix_nightly.py - Automate nightly for fulfillment team
 
-import ezsheets
+import pandas
 
-spreadsheet = ezsheets.Spreadsheet('1EwvByoi9LIQPsqwFraAE38xaghENWKhyI-_c1_3yWEM')
-print(spreadsheet.title)
-print("Hello world.")
+input_file = pandas.read_csv("./Excel/AllSales3.csv")
+keep_col = ['Name', 'Show', 'Performance Date', 'Confirmation Date', '# of Seats', 'Section', 'Row', 'Start',
+            'End']
+nightly_file = input_file[keep_col]
+nightly_file.to_csv("./Excel/AllSales3_Updated.csv", index=False)
