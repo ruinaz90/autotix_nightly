d43c8f82db066f4142aad7fb6fb561eacac00248
rzhao28 <arenzi28@outlook.com>
2020-06-03 19:53:57 -0400

    -

--- nightly/autotix_nightly.py
+++ nightly/autotix_nightly.py
@@ -1,40 +1,58 @@
 #! python3
 # autotix_nightly.py - Automate nightly for fulfillment team
 
+from pathlib import Path
 import pandas as pd
 import ezsheets
 import datetime
-import csv
 
-input_file = pd.read_csv("./Excel/AllSalesTest.csv")
-keep_col = ['Name', 'Show', 'Performance Date', 'Confirmation Date', '# of Seats', 'Section', 'Row', 'Start',
-            'End']
-nightly_file = input_file[keep_col].sort_values(by=['Performance Date', 'Show', 'Name'], ascending=True)
-nightly_file.to_csv("./Excel/AllSalesTest_Updated.csv", index=False)
 
-bway_txt = open("./Excel/AllSalesTest_Updated.csv", "r")
-bway_txt = "".join([i for i in bway_txt]).replace(" on Broadway", "")
-nightly_file = open("./Excel/AllSalesTest_Updated.csv", "w")
-nightly_file.writelines(bway_txt)
-nightly_file.close()
+def sort_columns(report_filename):
+    # Remove and sort columns
+    input_file = pd.read_csv(report_filename)
+    keep_col = ['Name', 'Show', 'Performance Date', 'Confirmation Date', '# of Seats', 'Section', 'Row', 'Start', 'End']
+    nightly_file = input_file[keep_col].sort_values(by=['Performance Date', 'Show', 'Name'], ascending=True)
+    nightly_file.to_csv(report_filename, index=False)
+    print("Sorting " + str(report_filename) + "...")
 
-ex_file = pd.read_csv("./Excel/AllSalesTest_Updated.csv")
-ex_file.to_clipboard(excel=True, index=False)
+    # Remove " on Broadway"
+    bway_txt = open(report_filename, 'r', encoding='utf8')
+    bway_txt = "".join([i for i in bway_txt]).replace(' on Broadway', '')
+    nightly_file = open(report_filename, 'w', encoding='utf8')
+    nightly_file.writelines(bway_txt)
+    nightly_file.close()
+    print("Cleaning up show names...")
 
-nightly_csv = open('./Excel/AllSalesTest_Updated.csv')
-nightly_reader = csv.reader(nightly_csv)
-nightly_data = list(nightly_reader)
 
-todays_date = datetime.datetime.now()
-todays_date_str = todays_date.strftime('%m/%d/%y')
+def copy_clipboard(report_filename):
+    # Copy report contents to clipboard
+    ex_file = pd.read_csv(report_filename)
+    ex_file.to_clipboard(excel=True, index=False)
+    print("Data copied to clipboard! Please paste into the Nightly Check-in doc!")
 
-nightly_gdoc = ezsheets.Spreadsheet('1EwvByoi9LIQPsqwFraAE38xaghENWKhyI-_c1_3yWEM')
-nightly_gdoc.createSheet(todays_date_str)
-current_sheet = nightly_gdoc[todays_date_str]
 
-i = 1
-for data in nightly_data:
-    current_sheet.updateRow(i, data)
-    i += 1
+def nightly_doc_tab():
+    # Get today's date
+    todays_date = datetime.datetime.now()
+    todays_date_str = todays_date.strftime('%m/%d/%y')
 
-nightly_gdoc.refresh()
+    # Create new tab in nightly doc and name it with today's date
+    nightly_gdoc = ezsheets.Spreadsheet('1EwvByoi9LIQPsqwFraAE38xaghENWKhyI-_c1_3yWEM')
+    nightly_gdoc.createSheet(todays_date_str)
+    print("New tab created in Nightly doc: " + todays_date_str + "...")
+
+
+def latest_file():
+    # Find the latest sales report file
+    folder_path = Path('C:/Users/Ruina/PycharmProjects/autotix/nightly/Excel')
+    list_of_paths = folder_path.glob('*.csv')
+    recent_file = max(list_of_paths, key=lambda p: p.stat().st_ctime)
+    print("Most recent report is "+ str(recent_file))
+    return recent_file
+
+
+# Run the program
+latest_path = latest_file()
+sort_columns(latest_path)
+nightly_doc_tab()
+copy_clipboard(latest_path)
---
+++ nightly/autotix_nightly_old.py
@@ -0,0 +1,40 @@
+#! python3
+# autotix_nightly.py - Automate nightly for fulfillment team
+
+import pandas as pd
+import ezsheets
+import datetime
+import csv
+
+input_file = pd.read_csv("./Excel/AllSales3.csv")
+keep_col = ['Name', 'Show', 'Performance Date', 'Confirmation Date', '# of Seats', 'Section', 'Row', 'Start',
+            'End']
+nightly_file = input_file[keep_col].sort_values(by=['Performance Date', 'Show', 'Name'], ascending=True)
+nightly_file.to_csv("./Excel/AllSalesTest_Updated.csv", index=False)
+
+bway_txt = open("./Excel/AllSalesTest_Updated.csv", "r", encoding="utf8")
+bway_txt = "".join([i for i in bway_txt]).replace(" on Broadway", "")
+nightly_file = open("./Excel/AllSalesTest_Updated.csv", "w", encoding="utf8")
+nightly_file.writelines(bway_txt)
+nightly_file.close()
+
+ex_file = pd.read_csv("./Excel/AllSalesTest_Updated.csv")
+ex_file.to_clipboard(excel=True, index=False)
+
+nightly_csv = open('./Excel/AllSalesTest_Updated.csv', encoding="utf8")
+nightly_reader = csv.reader(nightly_csv)
+nightly_data = list(nightly_reader)
+
+todays_date = datetime.datetime.now()
+todays_date_str = todays_date.strftime('%m/%d/%y')
+
+nightly_gdoc = ezsheets.Spreadsheet('1EwvByoi9LIQPsqwFraAE38xaghENWKhyI-_c1_3yWEM')
+nightly_gdoc.createSheet(todays_date_str)
+
+'''current_sheet = nightly_gdoc[todays_date_str]
+i = 1
+for data in nightly_data:
+    current_sheet.updateRow(i, data)
+    i += 1'''
+
+nightly_gdoc.refresh()
