150418d983a1f26f939a35227bf786b134584450
rzhao28 <arenzi28@outlook.com>
2020-06-03 13:03:05 -0400

    -

--- nightly/autotix_nightly.py
+++ nightly/autotix_nightly.py
@@ -6,5 +6,6 @@ import pandas
 input_file = pandas.read_csv("./Excel/AllSales3.csv")
 keep_col = ['Name', 'Show', 'Performance Date', 'Confirmation Date', '# of Seats', 'Section', 'Row', 'Start',
             'End']
-nightly_file = input_file[keep_col]
+
+nightly_file = input_file[keep_col].sort_values(by=['Performance Date', 'Show', 'Name'], ascending=True)
 nightly_file.to_csv("./Excel/AllSales3_Updated.csv", index=False)
