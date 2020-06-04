0a7bf854d8e42f64a789e0ccc8bf4969c322a131
rzhao28 <arenzi28@outlook.com>
2020-06-04 09:28:53 -0400

    -

--- nightly/test.py
+++ nightly/test.py
@@ -1,22 +1,8 @@
-import datetime
-import ezsheets
-import pyperclip
-import csv
+import sys
+from PyQt5 import QtWidgets, uic
 
-nightly_csv = open('./Excel/AllSalesTest_Updated.csv')
-nightly_reader = csv.reader(nightly_csv)
-nightly_data = list(nightly_reader)
+app = QtWidgets.QApplication(sys.argv)
 
-todays_date = datetime.datetime.now()
-todays_date_str = todays_date.strftime('%m/%d/%y')
-
-nightly_gdoc = ezsheets.Spreadsheet('1EwvByoi9LIQPsqwFraAE38xaghENWKhyI-_c1_3yWEM')
-nightly_gdoc.createSheet(todays_date_str)
-current_sheet = nightly_gdoc[todays_date_str]
-
-i = 1
-for data in nightly_data:
-    current_sheet.updateRow(i, data)
-    i += 1
-
-nightly_gdoc.refresh()
+window = uic.loadUi("mainwindow.ui")
+window.show()
+app.exec()
--- nightly/test2.py
+++ nightly/test2.py
@@ -15,3 +15,24 @@ for row in example_list:
     else:
         prev_show = current_show
         output_dict_writer.writerow({'Name': ''})
+
+
+
+
+nightly_csv = open('./Excel/AllSalesTest_Updated.csv')
+nightly_reader = csv.reader(nightly_csv)
+nightly_data = list(nightly_reader)
+
+todays_date = datetime.datetime.now()
+todays_date_str = todays_date.strftime('%m/%d/%y')
+
+nightly_gdoc = ezsheets.Spreadsheet('1EwvByoi9LIQPsqwFraAE38xaghENWKhyI-_c1_3yWEM')
+nightly_gdoc.createSheet(todays_date_str)
+current_sheet = nightly_gdoc[todays_date_str]
+
+i = 1
+for data in nightly_data:
+    current_sheet.updateRow(i, data)
+    i += 1
+
+nightly_gdoc.refresh()
