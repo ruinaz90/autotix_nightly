9e3bc1bb3dbebc2c92a201a3094d0783988e8885
rzhao28 <arenzi28@outlook.com>
2020-06-04 10:46:09 -0400

    -

--- nightly/test.py
+++ nightly/test.py
@@ -4,19 +4,24 @@ from PyQt5 import QtWidgets, uic
 from nightly.main_window import Ui_main_window
 
 
-class main_window(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
-        super(main_window, self).__init__(*args, **kwargs)
+        super(MainWindow, self).__init__(*args, **kwargs)
 
         # Load the UI Page
         uic.loadUi('mainwindow.ui', self)
 
 
+def run_clicked():
+    print("Run button clicked!")
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    main_win = main_window()
+    main_win = MainWindow()
     main_win.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
